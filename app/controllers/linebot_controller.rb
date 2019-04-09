09580be70a1f5898b22113fbecf43f7192b147ce
tarou <test@>
2019-04-09 16:03:08 +0900

    000

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -325,7 +325,7 @@ class LinebotController < ApplicationController
           if @@name&&@@payment&&@@image then
               @@image="#{SecureRandom.uuid}.jpg"
               image_response = client.get_message_content(event.message['id'])
-              tf = File.open("public/cost_images/#{@@image}", "w+b")
+              tf = File.open("#{Rails.public_path}/cost_images/#{@@image}", "w+b")
               tf.write(image_response.body)
               user=User.find_by(line_id:event['source']['userId'])
               cost=Cost.create(name:@@name,payment:@@payment,user_id:user.id,image_name: @@image)
@@ -344,7 +344,7 @@ class LinebotController < ApplicationController
 
                 @@payment=event.message['text'].tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z').gsub(/[^\d]/, "").to_i #半角にして、数字のみ抽出
                 @@image="仮"
-                warikan(cost)
+                #warikan(cost)
                 message = {
                   type: 'text',
                   text: '画像を送付してください'
